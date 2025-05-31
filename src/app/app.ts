8a1c55c630678eb3d921986c389c91f27ceae6ba
Tsvetan Tsvetkov <tsekach@gmail.com>
2025-05-31 21:00:27 +0200

    Add rate info

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BgnEuroCalculator } from './bgn-euro-calculator/bgn-euro-calculator';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle,
+} from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',
     styleUrl: './app.scss',
-    imports: [MatSidenavModule, MatToolbarModule, BgnEuroCalculator],
+    imports: [
+        MatSidenavModule,
+        MatToolbarModule,
+        BgnEuroCalculator,
+        MatAccordion,
+        MatExpansionPanel,
+        MatExpansionPanelHeader,
+        MatExpansionPanelTitle,
+        MatIcon,
+    ],
 })
 export class App {
     protected title = 'BG2EUR';
