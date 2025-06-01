0dda639607334115769dbbe8b058c15296c73113
Tsvetan Tsvetkov <tsekach@gmail.com>
2025-06-01 10:20:19 +0200

    Use default theme colors

--- src/app/app.ts
+++ src/app/app.ts
@@ -25,7 +25,4 @@ import { MatIcon } from '@angular/material/icon';
         MatIcon,
     ],
 })
-export class App {
-    protected title = 'BG2EUR';
-    protected readonly Array = Array;
-}
+export class App {}
